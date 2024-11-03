6cc52b34c69ef7cbee967bcf9b05864c3fb97d2f
NAYA <pianoman0903@naver.com>
2024-11-04 00:46:12 +0900

    해치웠나 ?

--- src/main/java/com/smhrd/Arti/Configuration/AwsS3Config.java
+++ src/main/java/com/smhrd/Arti/Configuration/AwsS3Config.java
@@ -1,21 +1,8 @@
 package com.smhrd.Arti.Configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsS3Config {
-
-	@Bean
-	S3Client s3Client() {
-		return S3Client.builder()
-				.region(Region.AP_NORTHEAST_2)
-				.credentialsProvider(ProfileCredentialsProvider.create())
-				.build();
-	}
 	
 }
--- src/main/java/com/smhrd/Arti/Service/S3Service.java
+++ src/main/java/com/smhrd/Arti/Service/S3Service.java
@@ -1,8 +1,29 @@
 package com.smhrd.Arti.Service;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
 public class S3Service {
 
+	private final AmazonS3 amazonS3;
+    private final String bucketName;
+
+    @Autowired
+    public S3Service(AmazonS3 amazonS3, @Value("${custom.s3.bucket-name}") String bucketName) {
+        this.amazonS3 = amazonS3;
+        this.bucketName = bucketName;
+    }
+
+    public void uploadFile(String key, File file) {
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, file);
+        amazonS3.putObject(putObjectRequest);
+    }
+    
 }
