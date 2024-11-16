ac792d09f171bc5d2d672c485657346493374600
capta <capta@DESKTOP-9RTT71H>
2024-11-16 20:42:50 +0900

    줄거리 수정중

--- src/main/java/com/smhrd/Arti/Model/Story.java
+++ src/main/java/com/smhrd/Arti/Model/Story.java
@@ -1,6 +1,9 @@
 package com.smhrd.Arti.Model;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,26 +12,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "tb_story")
 public class Story {
 
-	private Long uid;
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long b_id;
 	
+	private Long uid;
     private String b_writer;
     private String b_title;
     private String b_thumbnail;
     private String b_genre;
     private String b_Theme;
-    private Date b_create_date;
+    
+    @CreatedDate
+    private LocalDateTime b_create_date;
+    
+    public Story() {
+        this.b_create_date = LocalDateTime.now();
+    }
 	
 }
